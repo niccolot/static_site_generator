1da281f40ca56c0e15b875012ed2648823644ea6
niccolot <nico_tiezzi98hotmail.it>
2024-06-18 08:19:06 -0500

    split nodes delimiter function and some tests

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode
+from textnode import TextNode, split_nodes_delimiter, TextNodeType
 
 
 class TestTextNode(unittest.TestCase):
@@ -19,6 +19,33 @@ class TestTextNode(unittest.TestCase):
         self.assertNotEqual(node4, node5) # same text, same text type, different url with one as None
         self.assertNotEqual(node5, node6) # same text, same text type, different url
         self.assertEqual(node6, node7) # same text, same text type, same url
+
+
+class TextSplitNodesDelimiter(unittest.TestCase):
+    def test_func(self):
+        node1 = TextNode("This is text with a `code block` word", TextNodeType.text)
+        new_nodes1 = split_nodes_delimiter([node1], "`", TextNodeType.code)
+
+        expected_nodes1 = [
+                            TextNode("This is text with a ", TextNodeType.text),
+                            TextNode("code block", TextNodeType.code),
+                            TextNode(" word", TextNodeType.text),
+                        ]
+        
+        node2 = TextNode("This is a different text with more `code` inside", TextNodeType.text)
+        new_nodes2 = split_nodes_delimiter([node1, node2], "`", TextNodeType.code)
+
+        expected_nodes2 = [
+                            TextNode("This is text with a ", TextNodeType.text),
+                            TextNode("code block", TextNodeType.code),
+                            TextNode(" word", TextNodeType.text),
+                            TextNode("This is a different text with more ", TextNodeType.text),
+                            TextNode("code", TextNodeType.code),
+                            TextNode(" inside", TextNodeType.text),
+                        ]
+        
+        self.assertListEqual(new_nodes1, expected_nodes1)
+        self.assertListEqual(new_nodes2, expected_nodes2)
 
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -3,6 +3,8 @@ from enum import Enum
 
 from htmlnode import LeafNode
 
+TextNodeType = Enum('TextNodeType', ['text', 'bold', 'italic', 'code', 'link', 'image'])
+
 class TextNode:
     def __init__(self, text : str, text_type : str, url : str=None) -> None:
         self.text = text
@@ -18,8 +20,8 @@ class TextNode:
     def __repr__(self) -> str:
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
 
+
 def text_node_to_html_node(text_node : Type[TextNode]) -> Type[LeafNode]:
-    TextNodeType = Enum('TextNodeType', ['text', 'bold', 'italic', 'code', 'link', 'image'])
 
     if text_node.text_type == TextNodeType.text:
         return LeafNode(None, text_node.text, None)
@@ -44,3 +46,57 @@ def text_node_to_html_node(text_node : Type[TextNode]) -> Type[LeafNode]:
         return LeafNode("img", None, props)
     
     raise Exception('Invalid text node type')
+
+
+def is_valid_markdown(text : str, delimiter : str) -> bool:
+    if delimiter not in text:
+        return True
+    
+    delimiter_count = text.count(delimiter)
+    return delimiter_count % 2 == 0
+
+
+def delimiter_to_text_type(delimiter : str) -> TextNodeType:
+    if delimiter == "`":
+        return TextNodeType.code
+    
+    elif delimiter == "*":
+        return TextNodeType.italic
+    
+    elif delimiter == TextNodeType.bold:
+        return TextNodeType.bold
+    
+    else:
+        raise Exception("Invalid delimiter")
+
+
+def split_nodes_delimiter(old_nodes : list[Self], delimiter : str, text_type : Type[TextNodeType]) -> list[TextNode]:
+    def split_single_node(node : Self, delimiter : str, text_type : Type[TextNodeType]):
+        ret_list1 = []
+        text = node.text
+        if not is_valid_markdown(text, delimiter):
+            raise Exception(f"Invalid markdown delimiter syntax: matching {delimiter} character not found")
+        else: 
+            text_list = text.split(f"{delimiter}")
+            for i in range(len(text_list)):
+                # in a valid string the text enclosed by the delimiters (code, italic or bold) will be located on odd indexes 
+                # e.g. str_a = "*a*bcd*ef*" -> str_a.split("*") == ['', 'a', 'bcd', 'ef', ''] 
+                # str_b = "a*b*cd*ef*" -> str_b.split("*") == ['a', 'b', 'cd', 'ef', '']
+                if i % 2 != 0: 
+                    new_node = TextNode(text_list[i], delimiter_to_text_type(delimiter), node.url)
+                    ret_list1.append(new_node)
+                else:
+                    new_node = TextNode(text_list[i], TextNodeType.text, node.url)
+                    ret_list1.append(new_node)
+        
+        return ret_list1
+
+    ret_list = []
+    for node in old_nodes:
+        if (node.text_type is not TextNodeType.text) and (node.text_type is not TextNodeType.code):
+            ret_list.append(node)
+        else:
+            ret_list.extend(split_single_node(node, delimiter, text_type))
+    
+    return ret_list
+        
